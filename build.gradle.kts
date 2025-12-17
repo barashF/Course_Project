309c6424a870d6af9d6eb915341f134fe1367d0e
vitaly <vitaly@vitaly.vitaly>
2025-12-17 14:03:09 +0300

    fix gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,94 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
-    id("java")
+    java
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.example"
+version = "1.0"
+
+val javaVer = property("javaVersion") as String
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(javaVer))
+    }
+}
+
+javafx {
+    version = "17.0.2"
+    modules = listOf("javafx.controls", "javafx.fxml", "javafx.swing")
+}
+
+application {
+    mainClass.set("com.example.filesystemanalyzer.MainApp")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.openjfx:javafx-controls:17.0.2")
+    implementation("org.openjfx:javafx-fxml:17.0.2")
+    implementation("org.openjfx:javafx-swing:17.0.2")
+    implementation("org.apache.logging.log4j:log4j-api:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.20.0")
+    implementation("commons-io:commons-io:2.11.0")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
 }
 
-tasks.test {
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.release.set(javaVer.toInt())
+}
+
+tasks.withType<Test> {
     useJUnitPlatform()
+    testLogging {
+        events = setOf(TestLogEvent.PASSED, TestLogEvent.FAILED)
+    }
+}
+
+tasks {
+    named("distZip") {
+        dependsOn("shadowJar")
+    }
+    named("distTar") {
+        dependsOn("shadowJar")
+    }
+    named("startScripts") {
+        dependsOn("shadowJar")
+    }
+    named("startShadowScripts") {
+        dependsOn("jar")
+    }
+
+    named<ShadowJar>("shadowJar") {
+        archiveBaseName.set("FileSystemAnalyzer")
+        archiveClassifier.set("")
+        archiveVersion.set(version.toString())
+        mergeServiceFiles()
+
+        manifest {
+            attributes["Main-Class"] = "com.example.filesystemanalyzer.MainApp"
+            // Указываем модули JavaFX в аргументах JVM
+            attributes["Add-Opens"] = "java.base/java.lang=ALL-UNNAMED java.base/java.nio=ALL-UNNAMED"
+        }
+
+        // Включение всех зависимостей, включая JavaFX
+        configurations = listOf(project.configurations.runtimeClasspath.get())
+
+        // Исключение конфликтующих файлов
+        exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
+        exclude("META-INF/versions/**", "META-INF/maven/**", "META-INF/LICENSE*")
+        exclude("module-info.class")
+    }
 }
